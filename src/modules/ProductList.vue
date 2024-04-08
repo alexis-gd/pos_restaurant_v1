9598da0ce249025c31505c7e6cdfe4ec8ab02bc2
AlexisGD <katche4@gmail.com>
2024-04-08 12:38:13 -0600

    feat: add structure cards

--- src/modules/ProductList.vue
+++ src/modules/ProductList.vue
@@ -3,7 +3,9 @@
         <p>Categorías</p>
         <CategoriesSwiper></CategoriesSwiper>
     </div>
-    <ProductListItem></ProductListItem>
+    <template v-for="item in items" :key="item.id">
+        <ProductListItem :item="item"></ProductListItem>
+    </template>
 </template>
 
 <script>
@@ -20,7 +22,34 @@ export default {
         return {}
     },
     data() {
-        return {}
+        return {
+            items: [
+                {
+                    id: 1,
+                    image: 'url_de_la_imagen_1',
+                    title: 'Producto 1',
+                    description: 'Descripción del producto 1',
+                    price: 10.99,
+                    cta: 'Comprar ahora'
+                },
+                {
+                    id: 2,
+                    image: 'url_de_la_imagen_2',
+                    title: 'Producto 2',
+                    description: 'Descripción del producto 2',
+                    price: 19.99,
+                    cta: 'Agregar al carrito'
+                },
+                {
+                    id: 3,
+                    image: 'url_de_la_imagen_3',
+                    title: 'Producto 3',
+                    description: 'Descripción del producto 3',
+                    price: 15.99,
+                    cta: 'Ver detalles'
+                }
+            ]
+        }
     },
     mounted() { },
     methods: {}
--- src/modules/ProductListItem.vue
+++ src/modules/ProductListItem.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-
+    <div class="card" style="width: 18rem;">
+        <!-- <img :src="item.image" class="card-img-top" alt="Product Image"> -->
+        <div class="card-body">
+            <h5 class="card-title">{{ item.title }}</h5>
+            <p class="card-text">{{ item.description }}</p>
+            <p class="card-text">Precio: ${{ item.price }}</p>
+            <button class="btn btn-primary" @click="addToCart">{{ item.cta }}</button>
+        </div>
     </div>
 </template>
 
@@ -8,6 +14,12 @@
 
 export default {
     name: 'ProductListItem',
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
     components: {},
     setup() {
         return {}
@@ -16,7 +28,12 @@ export default {
         return {}
     },
     mounted() { },
-    methods: {}
+    methods: {
+        addToCart() {
+            // Implementa la lógica para agregar el producto al carrito
+            console.log('Agregado al carrito:', this.item.title);
+        }
+    }
 };
 </script>
 
