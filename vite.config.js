0d1c42ad699eba8d918c3c9b26831da5693708c6
AlexisGD <katche4@gmail.com>
2024-06-26 21:00:09 -0600

    intento de desplegar en pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      polyfills: ['es.promise', 'es.array.flat', 'es.object.entries'],
+    }),
     vue(),
   ],
+  base: '/restaurant/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
